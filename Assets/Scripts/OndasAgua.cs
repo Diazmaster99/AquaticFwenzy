a97f295b665faf782573e73f28a0ec2dc705fda3
informaticaesne <informatica@esne.es>
2024-01-17 09:59:07 +0100

    Cambios parallax y krill
    Se ha cambiado como funciona el parallax y como interactua el krill

---
+++ Assets/Scripts/OndasAgua.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OndasAgua : MonoBehaviour
+{
+    private Transform _transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
