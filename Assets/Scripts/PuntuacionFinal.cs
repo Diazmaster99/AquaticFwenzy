da98248524b9774f10bf6c4f33c366c57ce57190
informaticaesne <informatica@esne.es>
2024-01-17 08:51:12 +0100

    Puntuaje y puntuacion final
    Comentarios y codigo de puntuaje y ùntuacion final e implementacion en el menu win y en la escena de juego

--- Assets/Scripts/PuntuacionFinal.cs
+++ Assets/Scripts/PuntuacionFinal.cs
@@ -9,10 +9,11 @@ public class PuntuacionFinal : MonoBehaviour
     TMP_Text puntuacion;
 
     // Start is called before the first frame update
-    void OnEnable()
+    void OnEnable() //Se llama cuando el objeto se habilita y se ativa
     {
         puntuacion = GetComponent<TMP_Text>();
-        puntuacion.text = Mathf.RoundToInt(puntuaje.puntos).ToString();
+        puntuacion.text = Mathf.RoundToInt(puntuaje.puntos).ToString(); //Proporciona funciones matemeticas para poder trabajar con nº en punto flotante
+        //RoundToInt redondea un nº en punto flotante al entero mas cercano
     }
 
     // Update is called once per frame
