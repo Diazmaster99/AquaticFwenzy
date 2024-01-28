7af92a07127c8bb469f09619490ba22d0efbedfe
Andres <anfurtivo1@hotmail.com>
2024-01-28 02:25:15 +0100

    Cambiado condición victoria
    Ahora tienes que coger el huevo de cada nivel para poder pasar de nivel

--- Assets/Scripts/CondicionVictoria.cs
+++ Assets/Scripts/CondicionVictoria.cs
@@ -6,27 +6,76 @@ public class CondicionVictoria : MonoBehaviour
 {
     public GameObject menuwin;
     public float moveSpeed;
+    public float decelerationRate = 1;
+    private Rigidbody2D rb;
+    private bool final = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+
+        if (final)
+        {
+            Slow();
+        }
+        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "DestroyBoundary")
+        if (collision.gameObject.tag == "ProjectileBoundary")
         {
+            final = true;
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            
             menuwin.SetActive(true);
             Time.timeScale = 0f;
         }
     }
 
-    
-    
+    private void Slow()
+    {
+
+        moveSpeed = moveSpeed - 0.01f;
+
+        if (moveSpeed <= 0) 
+        {
+            moveSpeed = 0;
+        }
+
+        //rb.gravityScale -= decelerationRate * Time.deltaTime;
+
+        //if (rb.gravityScale<=0)
+        //{
+        //    moveSpeed = 0f;
+        //    rb.gravityScale = 0f;
+        //}
+
+        //// Slow down the object gradually
+        //if (rb.velocity.magnitude > 0.1f)
+        //{
+        //    rb.velocity -= rb.velocity.normalized * decelerationRate * Time.deltaTime;
+        //}
+        //else
+        //{
+        //    // Stop the object completely when its speed is very low
+        //    rb.velocity = Vector2.zero;
+        //}
+
+        
+
+    }
+
+
+
 }
