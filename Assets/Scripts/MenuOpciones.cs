12e15746f91e9327272e9c3d85bc69b8a90f546b
informaticaesne <informatica@esne.es>
2024-02-28 09:49:38 +0100

    Cambiado sistema resolucion
    Se ha cambiado el sistema que cambia la resolución, pero no se muestra correctamente en la interfaz

--- Assets/Scripts/MenuOpciones.cs
+++ Assets/Scripts/MenuOpciones.cs
@@ -1,15 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuOpciones : MonoBehaviour
 {
     //private bool fullscreen = false;
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+    List<Resolution> filteredResolutionList;
 
-    // Update is called once per frame
-    void Update()
+    private float currentRefreshRate;
+    private int currentResolutionIndex = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-        
+        //resolutions = Screen.resolutions;
+        //resolutionDropdown.ClearOptions();
+
+        //List<string> options = new List<string>();
+
+        //for (int i = 0; i < resolutions.Length; i++)
+        //{
+        //    string option = resolutions[i] + " x " + " height";
+        //    options.Add(option);
+        //}
+
+        //resolutionDropdown.AddOptions(options);
+
+        resolutions = Screen.resolutions;
+        filteredResolutionList = new List<Resolution>();
+
+        resolutionDropdown.ClearOptions();
+        currentRefreshRate = Screen.currentResolution.refreshRate;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            if (resolutions[i].refreshRate == currentRefreshRate)
+            {
+                filteredResolutionList.Add(resolutions[i]);
+            }
+        }
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < filteredResolutionList.Count; i++)
+        {
+            string resolutionOption = filteredResolutionList[i].width + "x" + filteredResolutionList[i].height + " " + filteredResolutionList[i].refreshRate + "Hz";
+            if (filteredResolutionList[i].width == Screen.width && filteredResolutionList[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
     }
 
     public void FullScreen(bool fullscreen) {
@@ -19,24 +71,26 @@ public class MenuOpciones : MonoBehaviour
 
     }
 
-    public void ChangeResolution(int numero)
+    public void ChangeResolution(int resolutionIndex)
     {
-        Debug.Log("resolution es: " + numero);
+        Resolution resolution = filteredResolutionList[resolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height,true);
+        Debug.Log("resolution es: " + resolutionIndex);
         //int numero = 1;
-        switch (numero)
-        {
-            case 0:
-                Screen.SetResolution(1920, 1080, true);
-                break;
-            case 1:
-                Screen.SetResolution(1420, 960, true);
-                break;
-            case 2:
-                //Screen.SetResolution(800, 600, false);
-                break;
-            default:
-                break;
-        }
+        //switch (resolutionIndex)
+        //{
+        //    case 0:
+        //        Screen.SetResolution(1920, 1080, true);
+        //        break;
+        //    case 1:
+        //        Screen.SetResolution(1420, 960, true);
+        //        break;
+        //    case 2:
+        //        //Screen.SetResolution(800, 600, false);
+        //        break;
+        //    default:
+        //        break;
+        //}
         
     }
 
