08dd7dd1bd346c583cfdce5b48cc43c14aa0f9d2
informaticaesne <informatica@esne.es>
2024-03-20 09:23:24 +0100

    Power Up Timer

--- Assets/Scripts/PowerUpTimer.cs
+++ Assets/Scripts/PowerUpTimer.cs
@@ -2,31 +2,46 @@ using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerUpTimer : MonoBehaviour
 {
-    public Image wheel;
+    public UnityEngine.UI.Image wheel;
+    public float tiempo;
+    public static float tiempoMax=0;
     // Start is called before the first frame update
     void Start()
     {
-        wheel = GetComponent<Image>();
+        wheel = GetComponent<UnityEngine.UI.Image>();
+        wheel.fillAmount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vaciar();
     }
 
-    public void Vaciar(int tiempo) 
+    public void Vaciar() 
     {
-        if (tiempo == 3)
+        //wheel.fillAmount = tiempo / tiempoMax;
+        if (tiempoMax == 3)
         {
-
+            tiempo += 0.0022f;
+            wheel.fillAmount = tiempo / tiempoMax;       
+            
         }
-        else 
+        if (tiempoMax == 6)
         {
-
+            tiempo += 0.0022f;
+            wheel.fillAmount = tiempo / tiempoMax;
+            
+        }
+        if (tiempo >= tiempoMax) 
+        {
+            tiempo = 0;
+            tiempoMax = 0;
+            wheel.fillAmount = 0;
         }
     }
 
